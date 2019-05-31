11797b89537e11a763a24f51686d3f17455662db
bunuli12138 <wuxinbei.work@gmail.com>
2019-05-31 12:29:55 +0800

    daily update May.31 from W.Y.

---
+++ ai_textProcess/Data/my_utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@Author     :xinbei
+@Software   :Pycharm
+@File       :my_utils.py
+@Time       :2019/5/31 11:17
+@Version    :
+@Purpose    :
+"""
+def adder(a, b):
+    return a + b
+
+
+if __name__ == '__main__':
+    pass
